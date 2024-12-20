0f19d8cfe215082d6bdbcc1b993ff7c7afefc003
vairabarath <lovelykdking@gmail.com>
2024-12-20 07:23:36 +0530

    some

--- frontend/src/components/Piechart.tsx
+++ frontend/src/components/Piechart.tsx
@@ -1,16 +1,19 @@
-import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from "recharts";
+import { PieChart, Pie, ResponsiveContainer } from "recharts";
 import { assets } from "../assets/assets";
 
 const data = [
-  { name: "Group A", value: 80, fill: "#C3EBFA" },
-  { name: "Group B", value: 20, fill: "#FFDCF1" },
+  { name: "Group A", value: 80, fill: "#BB86FC" },
+  { name: "Group B", value: 20, fill: "#03DAC6" },
 ];
 
 const Piechart = () => {
+  const greaterValue = data[0].value > data[1].value ? data[0] : data[1];
+  const textColor = greaterValue.fill;
+
   return (
-    <div className="bg-white rounded-md h-80 p-4 w-full relative">
+    <div className="bg-bgDark rounded-md h-80 p-4 w-full relative">
       <div className="flex justify-between items-center ">
-        <h1 className="text-xl font-semibold">Performance</h1>
+        <h1 className="text-xl font-semibold text-white">Performance</h1>
         <img src={assets.moreDark} alt="" width={16} height={16} />
       </div>
       <ResponsiveContainer width="100%" height="100%">
@@ -29,10 +32,12 @@ const Piechart = () => {
         </PieChart>
       </ResponsiveContainer>
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-        <h1 className="text-3xl font-bold">9.2</h1>
-        <p className="text-sm text-gray-400">good</p>
+        <h1 className="text-3xl font-bold" style={{ color: textColor }}>
+          9.2
+        </h1>
+        <p className="text-sm text-purple-300">good</p>
       </div>
-      <h2 className="absolute bottom-16 left-0 right-0 m-auto text-center font-medium">
+      <h2 className="absolute bottom-16 left-0 right-0 m-auto text-center font-medium text-white">
         Current Semester
       </h2>
     </div>
--- frontend/src/pages/Teacher.tsx
+++ frontend/src/pages/Teacher.tsx
@@ -1,9 +1,15 @@
-import { Calendar, CalendarCheck2, Droplets, Mail, Phone } from "lucide-react";
+import {
+  Calendar,
+  CalendarCheck2,
+  Droplets,
+  Mail,
+  MailIcon,
+  Phone,
+} from "lucide-react";
 import BigCalendar from "../components/BigCalendar";
 import Announcement from "../components/Announcement";
 import { Link } from "react-router-dom";
 import Piechart from "../components/Piechart";
-import TeacherForm from "../components/forms/TeacherForm";
 import Form from "../components/Form";
 
 const Teacher = () => {
@@ -11,7 +17,7 @@ const Teacher = () => {
     <div className="flex-1 p-4 flex gap-4 flex-col xl:flex-row">
       <div className="w-full xl:w-2/3">
         <div className="flex flex-col lg:flex-row gap-4 ">
-          <div className="bg-green py-6 px-4 rounded-md flex-1 flex gap-4">
+          <div className="bg-darkGreen py-6 px-4 rounded-md flex-1 flex gap-4">
             <div className="w-1/3">
               <img
                 src="https://images.pexels.com/photos/1043474/pexels-photo-1043474.jpeg?auto=compress&cs=tinysrgb&w=1200"
@@ -46,9 +52,14 @@ const Teacher = () => {
               </div>
 
               <p className="text-sm tetx-gray-500">
-                Loram ipsum dolor sit amet consectetur adipisicing elit.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quisquam quae voluptate, dolorum dolores quibusdam tempore quod.
+                Quae dolore inventore eum. Dolorum temporibus voluptatibus
+                natus. Sunt voluptatibus quidem. Quisquam quae voluptate,
+                dolorum dolores quibusdam tempore quod. Quae dolore inventore
+                eum.
               </p>
-              <div className="flex mt-28 items-center justify-between gap-2 flex-wrap text-xs font-medium">
+              <div className="flex mt-4 items-center justify-between gap-2 flex-wrap text-xs font-medium">
                 <div className="flex gap-2 items-center w-full md:w-1/3 lg:w-full 2xl:w-1/3 ">
                   <Droplets width={14} height={14} />
                   <span>O+</span>
@@ -58,7 +69,7 @@ const Teacher = () => {
                   <span>september 2004</span>
                 </div>
                 <div className="flex gap-2 items-center w-full md:w-1/3 lg:w-full 2xl:w-1/3 ">
-                  <Mail width={14} height={14} />
+                  <MailIcon width={14} height={14} />
                   <span>something@gmail.com</span>
                 </div>
                 <div className="flex gap-2 items-center w-full md:w-1/3 lg:w-full 2xl:w-1/3 ">
@@ -74,15 +85,17 @@ const Teacher = () => {
             <Piechart />
           </div>
         </div>
-        <div className="mt-4 bg-white rounded-md p-4 h-[800px]">
-          <h1>Teacher's Schedule</h1>
+        <div className="mt-4 bg-bgDark rounded-md p-4 h-[800px]">
+          <h1 className="text-xl font-semibold text-white">
+            Teacher's Schedule
+          </h1>
           <BigCalendar />
         </div>
       </div>
 
       <div className="w-full xl:w-1/3 flex flex-col gap-4">
-        <div className="bg-white p-4 rounded-md ">
-          <h1 className="text-xl font-semibold">Shortcuts</h1>
+        <div className="bg-bgDark p-4 rounded-md ">
+          <h1 className="text-xl font-semibold text-white">Shortcuts</h1>
           <div className="flex gap-4 mt-4 flex-wrap text-xs text-gray-500">
             <Link
               className="p-3 rounded-md bg-skyBlue"
@@ -118,14 +131,14 @@ const Teacher = () => {
         </div>
 
         <div className="flex gap-4 flex-1 justify-between  max-h-40 flex-wrap mb-4">
-          <div className="w-full bg-skyBlue max-h-40  p-4 rounded-md flex items-center justify-center gap-4 md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+          <div className="w-full bg-violet max-h-40  p-4 rounded-md flex items-center justify-center gap-4 md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
             <CalendarCheck2 width={24} height={24} className="w-6 h-6 " />
             <div>
               <h1 className="text-lg font-semibold">90%</h1>
               <span className="text-xs text-gray-500">Attendance</span>
             </div>
           </div>
-          <div className="w-full  max-h-40 bg-green p-4 rounded-md flex items-center justify-center gap-4 md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+          <div className="w-full  max-h-40 bg-darkGreen p-4 rounded-md flex items-center justify-center gap-4 md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
             <CalendarCheck2 width={24} height={24} className="w-6 h-6" />
             <div>
               <h1 className="text-lg font-semibold">80%</h1>
